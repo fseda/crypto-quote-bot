4726b291d50b1a0f5da701dd56f15a23cbe115b8
Felipe Seda <fsra1610@gmail.com>
2021-03-12 17:32:06 -0300

    fixed an import bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const config = require('./config.json');
 const Twit = require('twit');
-const tweetEvent = require('./services/TweetEventService');
+const tweetEvent = require('./services/tweetEventService');
 
 const { username } = require('./util/variables.json');
 
